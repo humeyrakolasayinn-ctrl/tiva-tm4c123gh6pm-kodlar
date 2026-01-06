363f26b191111cad21221b88f7a2e7ecd5058a4e
HUMEYRA <humeyrakolasayinn@gmail.com>
2026-01-06 20:29:17 +0300

    Update main.c

--- Odev1_LCD_Driver/main.c
+++ Odev1_LCD_Driver/main.c
@@ -1,1 +1,32 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+#include "inc/hw_ints.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "Lcd.h"
+
+
+
+int main(void) {
+
+        SysCtlClockSet(
+                        SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ
+                        | SYSCTL_OSC_MAIN);
+
+        Lcd_init();
+        Lcd_Goto(1,1);
+        Lcd_Puts("HUMEYRA ");
+        Lcd_Goto(2,4);
+        Lcd_Puts("SAU");
+
+        while (1) {
+
+        }
+}
+
 
